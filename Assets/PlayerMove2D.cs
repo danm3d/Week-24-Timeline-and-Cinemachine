5258cb62ca43da9960692f13dd24e4e28e7face8
Dan at Work <danmendelowitz@gmail.com>
2018-10-19 15:12:22 +0200

    Added a simple 2d rigidbody character controller

---
+++ Assets/PlayerMove2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove2D : MonoBehaviour
+{
+
+	protected Rigidbody2D body;
+	public float movementForce = 5f;
+	public float jumpForce = 5f;
+	private void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		float x = Input.GetAxis("Horizontal");
+
+		body.AddForce(Vector2.right * x * movementForce * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		}
+	}
+}
