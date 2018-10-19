e95ac6dd41b3da2f29025a8cf5892ebd81c7228b
Dan at Work <danmendelowitz@gmail.com>
2018-10-19 12:56:10 +0200

    Reverted some changed stuff

---
+++ Assets/Scripts/CinematicTankController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CinematicTankController : MonoBehaviour
+{
+	/// <summary>
+	/// The Transform to spawn projectiles/particles
+	/// </summary>
+	public Transform firingPoint;
+	/// <summary>
+	/// Particle system for the gun
+	/// </summary>
+	public ParticleSystem gunParticles;
+
+	/// <summary>
+	/// The movement speed of the tank
+	/// </summary>
+	public float speed = 5f;
+
+	public void FireGun()
+	{
+		if (EditorApplication.isPlaying)
+		{
+			Instantiate(gunParticles, firingPoint.position, firingPoint.rotation, firingPoint);
+		}
+	}
+
+	private void Update()
+	{
+		transform.Translate(transform.forward * speed * Time.deltaTime);
+	}
+}
